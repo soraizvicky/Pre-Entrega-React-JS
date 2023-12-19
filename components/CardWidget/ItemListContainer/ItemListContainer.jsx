3849a7d56bd3fbb7a42f86a3afbe73cde94d1e68
soraizvicky <126613385+soraizvicky@users.noreply.github.com>
2023-12-19 13:07:19 -0300

    ultima version

--- components/CardWidget/ItemListContainer/ItemListContainer.jsx
+++
@@ -1,9 +0,0 @@
-function ItemListContainer ({ greeting = "saludando por defecto"}) {
-    function ItemListContainer () {
-        return(
-           <div>
-            {greeting}
-           </div> 
-        )
-}
-export default ItemListContainer
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+React from "react" 6.9k (gzipped: 2.7k)
+reactDOM from 'react-dom/client' 513 (gzipped:320)
+App from './App.jsx'
+'./index.css'
+
+OM.createRoot( document.getElementById('root') ).render(<App/>)
